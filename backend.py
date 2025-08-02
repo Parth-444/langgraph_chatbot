58cc19f03a08a59ab956def04621a8cc059c0639
unknown <parthkannu0@gmail.com>
2025-08-02 14:06:34 +0530

    initial comit

---
+++ backend.py
@@ -0,0 +1,28 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.graph.message import add_messages
+import os
+
+key = os.getenv('GOOGLE_API_KEY_PRO')
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-pro", api_key=key)
+
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+def chat_node(state: ChatState):
+    message = state['messages']
+    response = llm.invoke(message)
+    return {"messages": [response]}
+
+checkpointer = InMemorySaver()
+
+graph = StateGraph(ChatState)
+
+graph.add_node("chat_node", chat_node)
+graph.add_edge(START, "chat_node")
+graph.add_edge("chat_node", END)
+
+chatbot = graph.compile(checkpointer)
