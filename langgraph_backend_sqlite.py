25ce4cb16dcea2aa793b340ca41b5151439dbf4f
unknown <parthkannu0@gmail.com>
2025-08-10 18:28:34 +0530

    Database

--- langgraph_backend_sqlite.py
+++ langgraph_backend_sqlite.py
@@ -1,10 +1,11 @@
 from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
-from langchain_core.messages import BaseMessage
+from langchain_core.messages import BaseMessage, HumanMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.checkpoint.sqlite import SqliteSaver
 from langgraph.graph.message import add_messages
 import os
+import sqlite3
 
 key = os.getenv('GOOGLE_API_KEY_PRO')
 llm = ChatGoogleGenerativeAI(model="gemini-2.5-pro", api_key=key)
@@ -17,7 +18,9 @@ def chat_node(state: ChatState):
     response = llm.invoke(message)
     return {"messages": [response]}
 
-checkpointer = InMemorySaver()
+conn = sqlite3.connect("chatbot.db", check_same_thread=False)
+
+checkpointer = SqliteSaver(conn=conn)
 
 graph = StateGraph(ChatState)
 
@@ -26,3 +29,9 @@ graph.add_edge(START, "chat_node")
 graph.add_edge("chat_node", END)
 
 chatbot = graph.compile(checkpointer)
+
+def retrieve_all_threads():
+    all_threads_set = set()
+    for checkpoints in checkpointer.list(None):
+        all_threads_set.add(checkpoints[0]['configurable']['thread_id'])
+    return list(all_threads_set)
